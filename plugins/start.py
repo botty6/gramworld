e293841148d4375325e8952950ff9c79ac5a784b
botty6 <135929682+botty6@users.noreply.github.com>
2025-04-10 17:13:10 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,7 +84,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 𝕚𝔹𝕆𝕏 𝕋𝕍', url='https://t.me/iBOX_TV')
+                    InlineKeyboardButton('🛠️ Dev', url='https://ibox-tv.com')
                 ]
             ]
                 )
